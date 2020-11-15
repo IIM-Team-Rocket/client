94a90cc218513a72503fe6435f6bccdb45c012c5
ptp <paartheepan94@gmail.com>
2020-11-16 00:36:27 +0100

    chore: remove workbox options

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,5 @@ module.exports = {
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'src/registerServiceWorker.js',
-      // ...other Workbox options...
-    },
   },
 };
