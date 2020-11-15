974d17af5c61e340092aaf72e38bf7ada5398f81
ptp <paartheepan94@gmail.com>
2020-11-16 00:25:28 +0100

    chore: add pwa config file

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  pwa: {
+    name: '2-mores',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/registerServiceWorker.js',
+      // ...other Workbox options...
+    },
+  },
+};
